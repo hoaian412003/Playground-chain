5a5b18084261284a1988d7a8999504dfeaebb8cb
tranhoaian <hoaian412003@gmail.com>
2023-01-12 11:09:23 +0700

    [Update] Bootnode start.sh

--- node/root/start-server.sh
+++ node/root/start-server.sh
@@ -1,6 +1,6 @@
 /usr/bin/geth init --datadir /run/chain/node/root /run/chain/node/genesis.json
 /usr/bin/geth --networkid 54321 --datadir /run/chain/node/root \
-  --bootnodes $(/run/chain/readEnv.sh ENODE) \
+  --bootnodes "enode://9854248dfaf98c5e01f65f3573d1bdda8b7df80cfd1c018f41147ee082ff3e796643618f05286c572efb79d52691457685157e6bf1121454fa465157f1ce32fc@127.0.0.1:0?discport=30301" \
   --port 30309 \
   --syncmode full \
   --allow-insecure-unlock --unlock a29b97db3276978d3c8cbac72086b95b42a0b716 \
