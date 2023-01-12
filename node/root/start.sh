696580f4f462d23036eba67c0b598398198a84b9
tranhoaian <hoaian412003@gmail.com>
2023-01-12 10:45:55 +0700

    [Fix] Log run

--- node/root/start.sh
+++ node/root/start.sh
@@ -1,2 +1,4 @@
 geth init --datadir root genesis.json
 geth --networkid 54321 --datadir root --bootnodes $(head -n 1 ./enode.txt) --port 30309 --syncmode full --allow-insecure-unlock --unlock a29b97db3276978d3c8cbac72086b95b42a0b716 --password root/password.txt --authrpc.port 2222 --http --http.api=eth,net,web3,personal,miner,txpool,admin --http.port 6868 --mine
+
+#&& /usr/bin/geth --networkid 54321 --datadir /run/chain/node/root --bootnodes $(./readEnv.sh ENODE) --port 30309 --syncmode full --allow-insecure-unlock --unlock a29b97db3276978d3c8cbac72086b95b42a0b716 --password /run/chain/node/root/password.txt --authrpc.port 2222 --http --http.api=eth,net,web3,personal,miner,txpool,admin --http.port 6868 --mine
