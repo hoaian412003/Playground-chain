667b75e9ccf2ade8a3b88ff99106e65848de3964
tranhoaian <hoaian412003@gmail.com>
2023-01-12 10:08:19 +0700

    [Update] Reload daemon in init.sh

--- init.sh
+++ init.sh
@@ -2,3 +2,6 @@
 sudo ln -s $(pwd) /run/chain
 # Link bootnode.service to bootnode.service
 sudo ln -s /run/chain/node/bootnode/bootnode.service /etc/systemd/system/bootnode.service
+
+# Reload daemon after init done source code
+sudo systemctl daemon-reload
