0624ecf710ebdacb99f7b79de27c212b2208cb63
tranhoaian <hoaian412003@gmail.com>
2023-01-12 10:33:17 +0700

    [Update] Add root service

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 sudo ln -s $(pwd) /run/chain
 # Link bootnode.service to bootnode.service
 sudo ln -s /run/chain/node/bootnode/bootnode.service /etc/systemd/system/bootnode.service
+sudo ln -s /run/chain/node/root/root.service /etc/systemd/system/root.service
 
 # Reload daemon after init done source code
 sudo systemctl daemon-reload
